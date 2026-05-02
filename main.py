61c99c1c5fb7e7a0f33ab85ca2b8a7f50781ccf6
hema071 <saadhema212@gmail.com>
2026-05-02 13:11:55 +0300

    kesin ve temiz kurulum

---
+++ main.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+import streamlit as st
+import sys
+
+# the prepration and the engine
+
+
+st.title ("Museum AI") # creates my website
+client = OpenAI(base_url = "https://openrouter.ai/api/v1",
+                api_key = "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")
+
+
+if "messages" not in st.session_state:
+    st.session_state.messages = [{"role": "system",
+           "content": "you are an assistant that answers in short messages and not long ones"}]
+
+
+if message := st.chat_input("what would you like to learn? "):
+    if message != "":
+        st.session_state.messages.append({"role": "user", "content": message})  # i saved my message as myself
+    if len(st.session_state.messages) > 10:
+        # memory.append({"role": "user",
+        #                "content": "summarize everything we have talked about above especially the most important stuff in maximum of 3 sentences"})
+        del st.session_state.messages[1]
+        del st.session_state.messages[1]
+        del st.session_state.messages[1]
+        del st.session_state.messages[1]
+
+    answer = client.chat.completions.create(model="openai/gpt-4o-mini", messages=st.session_state.messages)
+    answer = answer.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": answer})
+    st.markdown(answer)
+
+
+
+
+for one_message in st.session_state.messages[1:]:
+    with st.chat_message(one_message["role"]):
+        st.markdown(one_message["content"])
+
+
+
+
+
