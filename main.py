e732c9c98175ef7af35df3721209e55e0c3fe2ac
hema071 <saadhema212@gmail.com>
2026-05-03 15:18:23 +0300

    attempt to fix space sending

--- main.py
+++ main.py
@@ -11,22 +11,17 @@ client = OpenAI(
     api_key=st.secrets["MY_OPENROUTER_KEY"]
 )
 
-
-# i did a list called messages
-
 if "messages" not in st.session_state:
     st.session_state.messages = [{"role": "system",
            "content": "you are an assistant that answers in short messages and not long ones"}]
 
 
-
 if message := st.chat_input("what would you like to learn? "):
-    while True:
-        if message == " ":
+    if message != "":
+        if not message.strip():
             print ("empty message")
         else:
             st.session_state.messages.append({"role": "user", "content": message})  # i saved my message as myself
-            break
 
     answer = client.chat.completions.create(model="openai/gpt-4o-mini", messages=st.session_state.messages[-10:])
     answer = answer.choices[0].message.content
@@ -37,3 +32,8 @@ if message := st.chat_input("what would you like to learn? "):
 for one_message in st.session_state.messages[1:]:
     with st.chat_message(one_message["role"]):
         st.markdown(one_message["content"])
+
+
+
+
+
