99f2f42d49777a90493bf0d819d3370367361b15
hema071 <saadhema212@gmail.com>
2026-05-04 20:41:27 +0300

    split it into parts

---
+++ engine.py
@@ -0,0 +1,10 @@
+from openai import OpenAI
+
+def send(message_history):
+    client = OpenAI(base_url="https://openrouter.ai/api/v1",
+                    api_key=st.secrets["MY_OPENROUTER_KEY"])
+
+    answer = client.chat.completions.create(model="openai/gpt-4o-mini",
+                                            messages=message_history[-10:])
+    answer = answer.choices[0].message.content
+    return answer
--- main.py
+++ main.py
@@ -1,40 +1,25 @@
-from openai import OpenAI
 import streamlit as st
+import engine
 
-# i created my website
+def create():
+    st.title("Museum AI")
 
-st.title("Museum AI")
+    if "messages" not in st.session_state:
+        st.session_state.messages = [
+            {"role": "system", "content": "you are an assistant that answers in short messages and not long ones"}]
 
-# i connected the car engine
-client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=st.secrets["MY_OPENROUTER_KEY"])
-
-if "messages" not in st.session_state:
-    st.session_state.messages = [
-        {"role": "system", "content": "you are an assistant that answers in short messages and not long ones"}]
-
-    # i created the text box and the sending system
-
-if message := st.chat_input("what would you like to learn? "):
-    clean_message = message.strip()
-    if clean_message == "":
-        print("try again")
-
-        # i created the sending to engine system
-
-    else:
-        st.session_state.messages.append({"role": "user", "content": clean_message})  # i saved my message as myself
-        answer = client.chat.completions.create(model="openai/gpt-4o-mini",
-                                                messages=st.session_state.messages[-10:])
-        answer = answer.choices[0].message.content
-        st.session_state.messages.append({"role": "assistant", "content": answer})
-
-            # i prevented the disappearance
+    if message := st.chat_input("what would you like to learn? "):
+        clean_message = message.strip()
+        if clean_message == "":
+            print("try again")
+        else:
+            real_answer = engine.send(clean_message)
+            st.session_state.messages.append({"role": "assistant", "content": real_answer})
 
     for one_message in st.session_state.messages[1:]:
-       with st.chat_message(one_message["role"]):
-         st.markdown(one_message["content"])
+        with st.chat_message(one_message["role"]):
+            st.markdown(one_message["content"])
 
 
-
-
-
+if __name__ == "__main__":
+    create()
