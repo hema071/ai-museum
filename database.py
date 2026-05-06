7305325340fdadad7bc6e5e2df52ceabac7efb6e
hema071 <saadhema212@gmail.com>
2026-05-06 16:57:54 +0300

    bug

--- database.py
+++ database.py
@@ -4,11 +4,10 @@ import os
 def save(user_id, messages):
 
     file_path = f"C:/Users/hemas/OneDrive/Desktop/ai/{user_id}.json"
-
-    if not os.path.exists(file_path):
-        with open(file_path, "w") as file:  # ---------------> diffrent modes like w or x or a
-            json.dump(messages, file, indent=4)
-            print(f"txt file '{file_path}' was created")
+    
+    with open(file_path, "w") as file:  # ---------------> diffrent modes like w or x or a
+        json.dump(messages, file, indent=4)
+        print(f"txt file '{file_path}' was created")
 
 def load(user_id):
     file_path = f"C:/Users/hemas/OneDrive/Desktop/ai/{user_id}.json"
