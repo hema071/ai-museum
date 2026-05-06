4a4614de8dae04883c3418f84e7bd166c9fb117c
hema071 <saadhema212@gmail.com>
2026-05-06 16:56:09 +0300

    bug

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ def save(user_id, messages):
 
     file_path = f"C:/Users/hemas/OneDrive/Desktop/ai/{user_id}.json"
 
-    if os.path.exists(file_path):
+    if not os.path.exists(file_path):
         with open(file_path, "w") as file:  # ---------------> diffrent modes like w or x or a
             json.dump(messages, file, indent=4)
             print(f"txt file '{file_path}' was created")
