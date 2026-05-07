8839556a1abeca5d813b43d1f3c96e2c1efd9219
hema071 <saadhema212@gmail.com>
2026-05-07 16:24:24 +0300

    nothing much

--- database.py
+++ database.py
@@ -9,13 +9,14 @@ def save(user_id, messages):
 
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
-        print(f"Klasör bulunamadı, yeni oluşturuldu: {folder_path}")
+        print(f"Klasör bulunamadı yeni oluşturuldu: {folder_path}")
 
     file_path = f"{folder_path}/{user_id}.json"
 
     with open(file_path, "w") as file:  # ---------------> diffrent modes like w or x or a
         json.dump(messages, file, indent=4)
         print(f"txt file '{folder_path}' was created")
+        print("Dosyanın gerçek konumu:", os.path.abspath(file_path))
 
 def load(user_id):
     file_path = f"{folder_path}/{user_id}.json"
