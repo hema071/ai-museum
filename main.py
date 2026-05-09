78b459f3761ce4825fa68547c25c407423701a55
hema071 <saadhema212@gmail.com>
2026-05-09 11:42:03 +0300

    trial

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def create():
 
     if "messages" not in st.session_state:
         st.session_state.messages = [
-            {"role": "system", "content": "you are an assistant that answers in short messages and not long ones"}] # I gave the name messages
+            {}] # I gave the name messages
 
 
     if "mode" not in st.session_state:
@@ -21,11 +21,36 @@ def create():
 
     if "started" not in st.session_state:
         st.session_state.started = False
+        
+
+
+
+
+    @st.dialog
+    def popup():
+        st.title("Before continuing...")
+        name = st.text_input("What should we call you?")
+        mode = st.selectbox("Select your mode", ["Detailed", "Child", "Fast", "Default"])
+
+        if st.button ("lets begin"):
+            st.session_state.username = name
+            st.session_state.mode = mode
+            if st.session_state.mode == "Detailed":
+                st.session_state.messages.append ({"role": "system", "content": "you are an intelligent museum guide and your mission is to explain to the user everything they ask about detailed."})
+            elif st.session_state.mode == "Child":
+                st.session_state.messages.append ({"role": "system", "content": "you are an intelligent museum guide and your mission is explaining everything your user asks about in a simple and fun way. Your user is a child and you need to explain to them in an entertaining, simple and easy way."})
+            elif st.session_state.mode == "Fast":
+                st.session_state.messages.append ({"role": "system", "content": "you are an intelligent museum guide and your mission is explaining everything your user asks about quickly and using only short messages. you must not skip any important details"})
+            elif st.session_state.mode == "Default":
+                st.session_state.messages.append ({"role": "system", "content": "you are an intelligent museum guide and your mission is explaining everything your user asks about in a normal way. not too long, not too short and you must not skip any important details"})
+            
+            st.session_state.messages.append ({"role": "system", "content": f"your user's name is {st.session_state.username}"})
+            st.session_state.started = True
+            st.rerun()
 
     if not st.session_state.started:
-        st.dialog("before continuing...")
-        st.text_input ("What should we call you?")
-        st.selectbox ("Select your mode", ["Detailed", "Child", "Fast"])
+        popup()
+
 
 
 
