443f45a369ca1f63ef5b1d24ed2945ab14568acb
hema071 <saadhema212@gmail.com>
2026-05-17 11:15:42 +0300

    bug fixes

--- main.py
+++ main.py
@@ -64,13 +64,7 @@ def create():
             with st.chat_message(one_message["role"]):
                 st.markdown(one_message["content"])
 
-    @st.dialog("Are you sure?")
-    def verification():
-        if st.button("Yes"):
-            del st.session_state.messages[0]
-            del st.session_state.messages[1]
-            st.session_state.started = False
-            st.rerun()
+    
 
     if "id" not in st.query_params:
         st.query_params["id"] = str(uuid.uuid4())
@@ -96,6 +90,14 @@ def create():
             st.markdown("if you see empty page, please refresh and fill the information to have the access.")
         else:
             start()
+
+    @st.dialog("Are you sure?")
+    def verification():
+        if st.button("Yes"):
+            del st.session_state.messages[0]
+            del st.session_state.messages[1]
+            st.session_state.started = False
+            st.rerun()
 
     with st.sidebar:
         st.title("Settings")
